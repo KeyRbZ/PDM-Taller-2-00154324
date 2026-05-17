a56627a4e30276dc34a118d61290c932f7daf007
KeyRbZ <00154324@uca.edu.sv>
2026-05-17 10:41:56 -0600

    Feat: definir modelos de datos Dish y Restaurant

--- app/src/main/java/com/zelada/foodspot/model/Models.kt
+++ app/src/main/java/com/zelada/foodspot/model/Models.kt
@@ -1,2 +1,18 @@
 package com.zelada.foodspot.model
 
+data class Dish(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val imageUrl: String
+)
+
+
+data class Restaurant(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+    val categories: List<String>,
+    val menu: List<Dish>
+)
